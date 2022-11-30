24eaf84c55f738a412c3f87cfc4640a8edc6f963
kylekennedy26 <36827844+kylekennedy26@users.noreply.github.com>
2022-11-29 22:35:54 -0500

    added some starter code, working on installing the regex.posix library if we can even use it.

--- regexSearchReplace.hs
+++ regexSearchReplace.hs
@@ -1,1 +1,25 @@
+module FinalProj where
+
+import Data.Char
+import Data.List
+import Text.Regex.Posix
+
 --let the pain begin! :D
+
+main :: IO
+main = do
+    --prompt user for input
+    putStrLn "Welcome to RegEx Search and Replace. Enter a filename:"
+    filename <- getLine
+    putStrLn "Enter the string you want to find/match:"
+    matchString <- getLine
+    putStrLn "Enter the string you want to replace the found string with:"
+    replaceString <- getLine
+    --validate input? can we do this on one line?
+    validateInput userInput
+    fileContents <- readFile filename
+
+
+validateInput :: String -> String -> String -> String
+--x must be valid fileName, y must be valid string, z must be valid string.
+validateInput x y z = error "not implemented yet"
