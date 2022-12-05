fc5ca497e00f44d899ccb608c26444e3deb40805
Eli Orians <eli.orians@gmail.com>
2022-12-04 21:23:39 -0500

    some setup

--- regexSearchReplace.hs
+++ regexSearchReplace.hs
@@ -2,29 +2,42 @@ module FinalProj where
 
 import Data.Char
 import Data.List
---import Text.Regex.Posix
-
 
 data Regex = Letter Char | Concat Regex Regex | Choice Regex Regex |
              Lambda | Empty | Star Regex
 
-main :: IO
+main :: IO ()
 main = do
     --prompt user for input
     putStrLn "Welcome to RegEx Search and Replace. Enter a filename:"
     filename <- getLine
     putStrLn "Enter the string you want to find/match:"
-    matchString <- getLine
+    matchStr <- getLine
     putStrLn "Enter the string you want to replace the found string with:"
-    replaceString <- getLine
-    --validate input? can we do this on one line?
-    -- validateInput userInput
+    replaceStr <- getLine
+    --read file
     fileContents <- readFile filename
+    --do stuff
+    writeFile filename (runner fileContents matchStr replaceStr)
+    putStrLn ("Sucessfully replaced " ++ matchStr ++ " with " ++ replaceStr ++ " in " ++ filename)
 
 
-validateInput :: String -> String -> String -> String
---x must be valid fileName, y must be valid string, z must be valid string.
-validateInput x y z = error "not implemented yet"
+--the types below are placeholder and not correct i think
 
-match :: String -> Regex -> Bool
-match (s:ss) (Letter x) = 
+runner :: String -> String -> String -> String
+runner file matchStr replaceStr = error "not implemented"
+
+search :: String -> String
+search x = error "not implemented"
+
+replace :: String -> String
+replace x = error "not implemented"
+
+lexer :: String -> [Regex]
+lexer x = error "not implemented"
+
+parser :: [Regex] -> String
+parser x = error "not implemented"
+
+sr :: [Regex] -> [Regex] -> [Regex]
+sr x y = error "not implemented"
