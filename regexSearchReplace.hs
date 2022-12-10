c216015ab143f8523ee275d6fbfe6ca4cbdb6c7f
kylekennedy26 <36827844+kylekennedy26@users.noreply.github.com>
2022-12-09 21:59:02 -0500

    added helper functions for matchPrefix concat case to generate all tuples of a string but its wrong im dumb

--- regexSearchReplace.hs
+++ regexSearchReplace.hs
@@ -43,10 +43,21 @@ matchPrefix Empty str = []
 matchPrefix Lambda str = [str]
 matchPrefix (Letter ch) [] = [""]
 matchPrefix (Letter ch) (strChar:rst) = [rst | ch == strChar]
-matchPrefix (Concat r1 r2) str = if null list then list else [head list]
-                                    where list = [tail s2 | (s1, s2) <- splitString str, match r1 s1 && match r2 s2]
+--matchPrefix (Concat r1 r2) str = 
 matchPrefix (Choice r1 r2) str = matchPrefix r1 str ++ matchPrefix r2 str
-matchPrefix (Star r) str = matchPrefix r str
+matchPrefix (Star r) str = matchPrefix (Choice Lambda (Concat r (Star r))) str
+
+--helper functions for match prefix
+splitThree :: [a] -> [([a], [a], [a])]
+--generate all the possible tuples for s1 ++ s2 ++ s3 = str.
+--Repeated characters are treated as different. (Hello generates Hel twice since two ls.)
+splitThree str = [([x], [y], [z]) | (x, xs) <- pickOne str, (y, ys) <- pickOne xs, z <- ys]
+
+pickOne :: [a] -> [(a, [a])]
+pickOne [] = []
+pickOne (x : xs) = (x, xs) : [(y, x : ys) | (y, ys) <- pickOne xs]
+
+
 
 runProgram :: String -> String -> String -> String
 --takes as input 2 strings, and a string representing the filecontents as 1 huge string.
